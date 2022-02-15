f3d08bc00a6303cd6115d42fb8680f9103393f79
chuandong.huang <chuandong.huang@dmall>
2022-02-15 15:52:59 +0800

    add studentInfo views

--- src/views/admin/student/index.vue
+++ src/views/admin/student/index.vue
@@ -6,23 +6,28 @@
           <el-input placeholder="请输入姓名" @input="searchUser" v-model="searchParams">
             <i slot="prefix" class="el-input__icon el-icon-search"></i>
           </el-input>
-          <el-select placeholder="请选择学院" v-model="value">
+          <el-select placeholder="请选择学院" v-model="collegeValue">
             <el-option
               v-for="item in collegeList"
               :key="item.id"
-              :label="item.collegeStr"
-              :value="item.id"
+              :label="item.label"
+              :value="item.value"
             ></el-option>
           </el-select>
-          <el-select placeholder="请选择班级" v-model="value">
+          <el-select placeholder="请选择专业" v-model="vocationalValue">
             <el-option
-              v-for="item in collegeList"
+              v-for="item in vocationalList"
               :key="item.id"
-              :label="item.collegeStr"
-              :value="item.id"
+              :label="item.label"
+              :value="item.value"
             ></el-option>
           </el-select>
-          <el-button type="primary" :disabled="accountType == 1 ? false : true" size="mini">搜索</el-button>
+          <el-button
+            type="primary"
+            :disabled="accountType == 1 ? false : true"
+            size="mini"
+            @click="handleSearch"
+          >搜索</el-button>
         </div>
         <div class="addBtn">
           <el-button
@@ -43,6 +48,7 @@
       <el-table-column prop="phone" label="电话" width="120"></el-table-column>
       <el-table-column prop="idCard" label="身份证号码" width="180"></el-table-column>
       <el-table-column prop="collegeId" label="学院"></el-table-column>
+      <el-table-column prop="vocationalId" label="专业"></el-table-column>
       <el-table-column prop="classId" label="班级"></el-table-column>
       <el-table-column prop="hostelId" label="宿舍"></el-table-column>
       <el-table-column prop="ethnic" label="民族"></el-table-column>
@@ -68,50 +74,26 @@
       layout="->,total, sizes, prev, pager,next, jumper"
       :total="pagination.total || 0"
     ></el-pagination>
-    <el-drawer
-      title="添加学生信息"
-      :visible.sync="addEditVisible"
-      :direction="direction"
-      :before-close="handleClose"
-      size="40%"
-    >
+    <el-drawer title="添加学生信息" :visible.sync="addEditVisible" size="40%">
       <el-form :model="studentInfo" :rules="studentInfoRules" ref="studentInfo" label-width="100px">
-        <el-form-item label="用户名" prop="name">
+        <el-form-item label="姓名：" prop="name">
           <el-input v-model="studentInfo.name"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="sex">
-          <el-input v-model="studentInfo.sex"></el-input>
+        <el-form-item label="性别：" prop="sex">
+          <el-radio v-model="studentInfo.sex" label="1">男</el-radio>
+          <el-radio v-model="studentInfo.sex" label="2">女</el-radio>
         </el-form-item>
-        <el-form-item label="邮箱" prop="age">
+        <el-form-item label="年龄:" prop="age">
           <el-input v-model="studentInfo.age"></el-input>
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="studentInfo.phone"></el-input>
+        <el-form-item label="电话:" prop="phone">
+          <el-input v-model="studentInfo.phone" maxlength="11"></el-input>
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="studentInfo.phone"></el-input>
+        <el-form-item label="身份证号码:" prop="idCard">
+          <el-input v-model="studentInfo.idCard" maxlength="18"></el-input>
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="studentInfo.phone"></el-input>
-        </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="studentInfo.phone"></el-input>
-        </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="studentInfo.phone"></el-input>
-        </el-form-item>
-        <!-- <el-form-item label="角色类型" prop="accountType" :v-if="accountType == 1">
-          <el-select v-model="userInfo.accountType" placeholder="请选择">
-            <el-option
-              v-for="item in accountTypeEnum"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="所属学院" prop="collegeId" :v-if="accountType == 1">
-          <el-select v-model="userInfo.collegeId" placeholder="请选择">
+        <el-form-item label="所属学院" prop="collegeId">
+          <el-select v-model="studentInfo.collegeId" placeholder="请选择">
             <el-option
               v-for="item in collegeList"
               :key="item.value"
@@ -119,21 +101,76 @@
               :value="item.value"
             ></el-option>
           </el-select>
-        </el-form-item>-->
-        <!-- <el-form-item label="状态" prop="status" :v-if="accountType == 1">
-          <el-switch v-model="userInfo.status" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
-        </el-form-item>-->
+        </el-form-item>
+        <el-form-item label="所属专业" prop="vocationalId">
+          <el-select v-model="studentInfo.vocationalId" placeholder="请选择">
+            <el-option
+              v-for="item in vocationalList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="所在班级" prop="classId">
+          <el-select v-model="studentInfo.classId" placeholder="请选择">
+            <el-option
+              v-for="item in collegeList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="宿舍:" prop="hostelId">
+          <el-input v-model="studentInfo.hostelId"></el-input>
+        </el-form-item>
+        <el-form-item label="民族" prop="ethnic">
+          <el-select v-model="studentInfo.ethnic" placeholder="请选择">
+            <el-option
+              v-for="item in nationList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="生源地:" prop="birthPlace">
+          <el-input v-model="studentInfo.birthPlace"></el-input>
+        </el-form-item>
+        <el-form-item label="家庭住址:" prop="address">
+          <el-input v-model="studentInfo.address" type="textarea"></el-input>
+        </el-form-item>
+        <el-form-item label="毕业院校:" prop="graduate">
+          <el-input v-model="studentInfo.graduate"></el-input>
+        </el-form-item>
+        <el-form-item label="辅导员姓名:" prop="counselorId">
+          <el-select v-model="studentInfo.counselorId" placeholder="请选择">
+            <el-option
+              v-for="item in collegeList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="辅导员电话:" prop="counselorPhone">
+          <el-input v-model="studentInfo.counselorPhone" disabled></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleAddEdit">添加</el-button>
+          <el-button @click="handleCancel">取消</el-button>
+        </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="handleCancel">取 消</el-button>
-        <el-button type="primary" @click="submit">确 定</el-button>
-      </div>
     </el-drawer>
   </div>
 </template>
 
 <script>
-import { collegeList, studentsList } from '../../../api/students'
+import { studentsList } from '@/api/students'
+import { collegeList } from '@/api/college'
+import { vocationalList } from '@/api/vocational'
+import { nationList } from '@/util/Enum'
 // import { mapState } from 'vuex'
 export default {
   components: {},
@@ -141,13 +178,15 @@ export default {
     return {
       studentsList: [],
       collegeList: [],
+      vocationalList: [],
       searchParams: '',
       accountType: 1,
       pagination: {
         pageSize: 10,
         currentPage: 1,
       },
-      value: '',
+      collegeValue: '',
+      vocationalValue: '',
       addEditVisible: false,
       studentInfo: {
         name: '',
@@ -157,12 +196,53 @@ export default {
         idCard: '',
         collegeId: '',
       },
+      nationList,
+      studentInfoRules: {
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' },
+          { min: 2, max: 8, message: '姓名长度为2~8', trigger: 'blur' },
+        ],
+        sex: [{ required: true, message: '请选择性别', trigger: 'change' }],
+        age: [{ required: true, message: '请输入年龄', trigger: 'blur' }],
+        phone: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { max: 11, message: '请输入长度为11的手机号码', trigger: 'blur' },
+        ],
+        idCard: [
+          { required: true, message: '请输入身份证号码', trigger: 'blur' },
+          { max: 18, message: '请输入长度为18的身份证号码', trigger: 'blur' },
+        ],
+        collegeId: [
+          { required: true, message: '请选择学院', trigger: 'change' },
+        ],
+        vocationalId: [
+          { required: true, message: '请选择专业', trigger: 'change' },
+        ],
+        classId: [{ required: true, message: '请选择班级', trigger: 'change' }],
+        hostelId: [
+          { required: true, message: '请输入宿舍号', trigger: 'blur' },
+        ],
+        classId: [{ required: true, message: '请选择民族', trigger: 'change' }],
+        birthPlace: [
+          { required: true, message: '请输入生源地', trigger: 'blur' },
+        ],
+        address: [
+          { required: true, message: '请输入家庭住址', trigger: 'blur' },
+        ],
+        graduate: [
+          { required: true, message: '请输入毕业院校', trigger: 'blur' },
+        ],
+        counselorId: [
+          { required: true, message: '请选择辅导员', trigger: 'change' },
+        ],
+      },
     }
   },
   computed: {},
   watch: {},
   created() {
     this.getCollegeList()
+    this.getVocational()
     this.getStudentList()
   },
   mounted() {},
@@ -172,8 +252,22 @@ export default {
     // 获取学院列表
     async getCollegeList() {
       const { data } = await collegeList()
-      this.collegeList = data.data
+      this.collegeList = data.data.map((x) => ({
+        ...x,
+        label: x.collegeStr,
+        value: x.id,
+      }))
     },
+    // 获取专业列表
+    async getVocational() {
+      const { data } = await vocationalList()
+      this.vocationalList = data.data.map((x) => ({
+        ...x,
+        label: x.vocationalStr,
+        value: x.id,
+      }))
+    },
+    // 学生信息
     async getStudentList() {
       const { data } = await studentsList(this.pagination)
       console.log(data)
@@ -182,15 +276,27 @@ export default {
       }))
       this.pagination = data.data.pagination
     },
+    // 处理搜索
+    handleSearch() {},
     openAddEditDrawer() {
       this.addEditVisible = true
     },
-    handleAddEdit() {},
+    handleAddEdit(row) {
+      if (row) {
+        // 编辑
+      } else {
+        // 修改
+      }
+    },
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`)
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`)
+    },
+    handleCancel() {
+      this.$refs[studentInfo].resetFields()
+      this.addEditVisible = false
     },
   },
 }
