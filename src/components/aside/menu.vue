b67e3ed42a2fea2e58fd00c41aa3774e950a6376
chuandong.huang <chuandong.huang@dmall>
2022-03-21 15:32:30 +0800

    feat router config

--- src/components/aside/menu.vue
+++ src/components/aside/menu.vue
@@ -14,10 +14,10 @@
       <el-menu-item index="4">财务管理</el-menu-item>
       <el-submenu index="5">
         <template slot="title">教务管理</template>
-        <el-menu-item index="5-1">学院管理</el-menu-item>
-        <el-menu-item index="5-2">专业管理</el-menu-item>
-        <el-menu-item index="5-3">班级管理</el-menu-item>
-        <el-menu-item index="5-4">教师管理</el-menu-item>
+        <el-menu-item index="/admin/EducationalAffairs/college">学院管理</el-menu-item>
+        <el-menu-item index="/admin/EducationalAffairs/vocational">专业管理</el-menu-item>
+        <el-menu-item index="/admin/EducationalAffairs/class">班级管理</el-menu-item>
+        <el-menu-item index="/admin/EducationalAffairs/counselor">教师管理</el-menu-item>
       </el-submenu>
     </el-menu>
   </div>
@@ -31,57 +31,57 @@ export default {
       menuList: [
         //admin
         {
-          title: "管理员管理",
-          url: "/admin/users",
-          icon: "el-icon-user-solid",
+          title: '管理员管理',
+          url: '/admin/users',
+          icon: 'el-icon-user-solid',
           key: 1,
         },
         // student
         {
-          title: "学生信息管理",
-          url: "/admin/students",
-          icon: "el-icon-s-management",
+          title: '学生信息管理',
+          url: '/admin/students',
+          icon: 'el-icon-s-management',
           key: 2,
         },
         //hostel
         {
-          title: "学生宿舍管理",
-          url: "/admin/hotel",
-          icon: "el-icon-s-home",
+          title: '学生宿舍管理',
+          url: '/admin/hotel',
+          icon: 'el-icon-s-home',
           key: 3,
         },
         // financial
         {
-          title: "财务管理",
-          url: "/admin/finance",
-          icon: "el-icon-s-finance",
+          title: '财务管理',
+          url: '/admin/finance',
+          icon: 'el-icon-s-finance',
           key: 4,
         },
         // school
         {
-          title: "教务管理",
-          url: "/admin/college",
-          icon: "el-icon-s-tools",
+          title: '教务管理',
+          url: '/admin/college',
+          icon: 'el-icon-s-tools',
           key: 5,
           subMenu: [
-            { title: "学院管理", url: "/admin/users", key: 5 - 1 },
-            { title: "专业管理", url: "/admin/users", key: 5 - 2 },
-            { title: "班级管理", url: "/admin/users", key: 5 - 3 },
-            { title: "辅导员管理", url: "/admin/users", key: 5 - 4 },
+            { title: '学院管理', url: '/admin/users', key: 5 - 1 },
+            { title: '专业管理', url: '/admin/users', key: 5 - 2 },
+            { title: '班级管理', url: '/admin/users', key: 5 - 3 },
+            { title: '辅导员管理', url: '/admin/users', key: 5 - 4 },
           ],
         },
       ],
-    };
+    }
   },
   computed: {
     activeUsers: function () {
-      let subMenu = [];
+      let subMenu = []
       for (let i = 0; i < this.menuList.length; i++) {
         if (this.menuList[i].subMenu) {
-          subMenu = this.menuList[i].subMenu;
+          subMenu = this.menuList[i].subMenu
         }
       }
-      return subMenu;
+      return subMenu
     },
   },
   watch: {},
@@ -89,7 +89,7 @@ export default {
   created() {},
   mounted() {},
   updated() {},
-};
+}
 </script>
 <style lang="less" scoped>
 .left-menu {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from "../views/admin/login.vue";
+import Login from "@/views/admin/login.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,32 +16,55 @@ const routes = [
     path: "/admin",
     name: "admin",
     component: () =>
-      import(/* webpackChunkName: "admin" */ "../views/admin/index.vue"),
+      import(/* webpackChunkName: "admin" */ "@/views/admin/index.vue"),
     children: [
       {
         path: "users",
         name: "users",
-        component: () => import("../views/admin/users/index.vue"),
+        component: () => import("@/views/admin/users/index.vue"),
       },
       {
         path: "students",
         name: "students",
-        component: () => import("../views/admin/student/index.vue"),
+        component: () => import("@/views/admin/student/index.vue"),
       },
       {
         path: "hotel",
         name: "hotel",
-        component: () => import("../views/admin/hostel/index.vue"),
+        component: () => import("@/views/admin/hostel/index.vue"),
       },
       {
         path: "finance",
         name: "finance",
-        component: () => import("../views/admin/users/index.vue"),
+        component: () => import("@/views/admin/users/index.vue"),
       },
+      // 教务管理
       {
-        path: "college",
-        name: "finance",
-        component: () => import("../views/admin/users/index.vue"),
+        path: "EducationalAffairs",
+        name: "EducationalAffairs",
+        component: () => import(/* webpackChunkName: "EducationalAffairs" */ "@/views/admin/EducationalAffairs/index.vue"),
+        children: [
+          {
+            path: "class",
+            name: "class",
+            component: () => import("@/views/admin/EducationalAffairs/class/index.vue"),
+          },
+          {
+            path: "college",
+            name: "college",
+            component: () => import("@/views/admin/EducationalAffairs/college/index.vue"),
+          },
+          {
+            path: "counselor",
+            name: "counselor",
+            component: () => import("@/views/admin/EducationalAffairs/counselor/index.vue"),
+          },
+          {
+            path: "vocational",
+            name: "vocational",
+            component: () => import("@/views/admin/EducationalAffairs/vocational/index.vue"),
+          },
+        ]
       },
     ],
   },
