0ee573bd076cd4d71e031bf6b4191842d217e21a
chuandong.huang <chuandong.huang@dmall>
2022-03-21 15:33:43 +0800

    feat(views): feat EducationalAffairs

---
+++ src/views/admin/EducationalAffairs/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputParams: "",
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="less" scoped></style>
